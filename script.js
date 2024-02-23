8919760c26527d2263f4005bedeaa3da1192fbfa
jatiner <60155235+jatiner@users.noreply.github.com>
2024-02-23 17:20:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<nav class="art-folders">
+    <a href="folder1/"> 
+        <img src="images/folder1-preview.jpg" alt="Folder 1">
+        Folder 1 Title 
+    </a>
+    <a href="folder2/">
+        </a>
+    </nav>
