cfb8b6975a6410617ae429945e0b8f5a312b07be
jatiner <60155235+jatiner@users.noreply.github.com>
2024-02-24 21:54:52 -0600

    Update script.js

--- images/folder3/script.js
+++ images/folder3/script.js
@@ -1,5 +1,5 @@
 const imageContainer = document.querySelector('.gallery-container');
-const imageNames = ["LightVoid 1.png", "LightVoid 2.png", "LightVoid 3.png", "LightVoid 4.png", "LightVoid 5.png", "LightVoid 6.png", "LightVoid 7.png", "LightVoid 8.png", "LightVoid 9.png", "LightVoid 10.png" /* ... add your 10 image names */];
+const imageNames = ["LightVoid 1.png", "LightVoid 2.png", "LightVoid 3.png", "LightVoid 4.png", "LightVoid 5.png", "LightVoid 6.png", "LightVoid 8.png", "LightVoid 9.png", "LightVoid 10.png" /* ... add your 10 image names */];
 
 imageNames.forEach(imageName => {
     const img = document.createElement('img');
