434bd3469d3a5bb1e115bfec931d71c2f486ffb2
jatiner <60155235+jatiner@users.noreply.github.com>
2024-02-24 19:03:11 -0600

    Update script.js

--- images/folder1/script.js
+++ images/folder1/script.js
@@ -1,5 +1,5 @@
 const imageContainer = document.querySelector('.gallery-container');
-const imageNames = ["SpringPattern3", "SpringPattern4", "SpringPattern6", "SpringPattern8", "SpringPattern9", "SpringPattern10", "SpringPattern12", "SpringPattern14", "SpringPattern16", "SpringPattern21"/* ... add your 10 image names */];
+const imageNames = ["SpringPattern3.png", "SpringPattern4.png", "SpringPattern6.png", "SpringPattern8.png", "SpringPattern9.png", "SpringPattern10.png", "SpringPattern12.png", "SpringPattern14.png", "SpringPattern16.png", "SpringPattern21.png"/* ... add your 10 image names */];
 
 imageNames.forEach(imageName => {
     const img = document.createElement('img');
