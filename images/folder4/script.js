25924e2cf62fc217606bde654d39410c889eafb6
jatiner <60155235+jatiner@users.noreply.github.com>
2024-02-24 23:20:40 -0600

    Update script.js

--- images/folder4/script.js
+++ images/folder4/script.js
@@ -1,5 +1,5 @@
 const imageContainer = document.querySelector('.gallery-container');
-const imageNames = ["image1.jpg", "image2.jpg", /* ... add your 10 image names */];
+const imageNames = ["Norse1.png", "Norse2.png", "Norse3.png", "Norse5.png", "Norse6.png", "Norse7.png", "Norse8.png", "Norse9.png", "Norse10.png"/* ... add your 10 image names */];
 
 imageNames.forEach(imageName => {
     const img = document.createElement('img');
